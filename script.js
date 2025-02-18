1be33440e6aa69d20994a0ee5cce961d9a4eab33
Shaun Stummer <shaun.stummer1@tafensw.edu.au>
2025-02-18 14:46:36 +1100

    Updated wk3

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-document.addEventListener('DOMContentLoaded',function(){
-        //alert("The page loaded");
-    let count = 0;
-    var button = document.getElementById('myButton');
-    button.addEventListener('click', function(){
-        //alert('You clicked the button!');
-        var paragraph = document.getElementById('somePara');
-        count += 1; //count = count + 1;
-        paragraph.textContent = `Count is: ${count}`;
-    })
-})
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('myForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        var name = document.getElementById('nameInput').value;
+        var email = document.getElementById('emailInput').value;
+        var paragraph = document.getElementById('somePara')
+        paragraph.textContent = 'Form submitted with Name: ' + name + ' and Email: ' + email;
+        //alert('Form submitted with Name: ' + name + ' and Email: ' + email);
+    });
+});
+
+
+
+// document.addEventListener('DOMContentLoaded',function(){
+//         //alert("The page loaded");
+//     let count = 0;
+//     var button = document.getElementById('myButton');
+//     button.addEventListener('click', function(){
+//         //alert('You clicked the button!');
+//         var paragraph = document.getElementById('somePara');
+//         count += 1; //count = count + 1;
+//         paragraph.textContent = `Count is: ${count}`;
+//     })
+// })
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     var paragraph = document.querySelector('p');
